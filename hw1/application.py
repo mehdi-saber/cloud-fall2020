25f1ca893ce787ee6232347dadfea19fdf76d404
mehdi <mehdi.saber@cafebazaar.ir>
2020-11-17 23:13:50 +0330

    Adds functions.

--- hw1/application.py
+++ hw1/application.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
-from utils import vms_list, vm_info
+from utils import vms_list, vm_info, vm_stop, vm_start, vm_change_memory, vm_change_cpu, vm_delete, vm_clone
 
 app = Flask(__name__
             , static_url_path='',
@@ -10,13 +10,97 @@ app = Flask(__name__
 
 
 @app.route('/dashboard', methods=['GET'])
-def get_main_page():
+@app.route('/', methods=['GET'])
+def get_dashboard():
     vms = vms_list()
     vms_info = [vm_info(vm) for vm in vms]
     return render_template('main.html', vms_info=vms_info)
 
 
-#     return redirect("<url>" , code=302)
+@app.route('/start/<vm_name>', methods=['GET'])
+def start_vm(vm_name):
+    vm_start(vm_name)
+    return redirect("/", code=302)
+
+
+@app.route('/power-off/<vm_name>', methods=['GET'])
+def stop_vm(vm_name):
+    vm_stop(vm_name)
+    return redirect("/", code=302)
+
+
+@app.route('/change-ram/<vm_name>', methods=['GET'])
+def change_vm_ram(vm_name):
+    current_ram = vm_info(vm_name)['memory']
+    info = vm_info(vm_name)
+    current_ram = int(current_ram.replace('MB', ''))
+    messages = []
+    if info['state'] != 'powered off':
+        messages.append("Vm is running it will be turned off for change!")
+    return render_template('edit-ram.html', current_ram=current_ram, vm_name=vm_name, messages=messages)
+
+
+@app.route('/change-ram/', methods=['POST'])
+def change_vm_ram_post():
+    vm_name = request.form['vm-name']
+    info = vm_info(vm_name)
+    memory = request.form['memory']
+    if info['state'] != 'powered off':
+        vm_stop(vm_name)
+    vm_change_memory(vm_name, memory)
+    return redirect("/", code=302)
+
+
+@app.route('/change-cpu/<vm_name>', methods=['GET'])
+def change_vm_cpu(vm_name):
+    current_cpu = vm_info(vm_name)['cpu']
+    info = vm_info(vm_name)
+    current_cpu = int(current_cpu.replace('MB', ''))
+    messages = []
+    if info['state'] != 'powered off':
+        messages.append("Vm is running it will be turned off for change!")
+    return render_template('edit-cpu.html', current_cpu=current_cpu, vm_name=vm_name, messages=messages)
+
+
+@app.route('/change-cpu/', methods=['POST'])
+def change_vm_cpu_post():
+    vm_name = request.form['vm-name']
+    info = vm_info(vm_name)
+    cores = request.form['cores']
+    if info['state'] != 'powered off':
+        vm_stop(vm_name)
+    vm_change_cpu(vm_name, cores)
+    return redirect("/", code=302)
+
+
+@app.route('/delete/<vm_name>', methods=['GET'])
+def change_vm_delete(vm_name):
+    info = vm_info(vm_name)
+    if info['state'] != 'powered off':
+        vm_stop(vm_name)
+    vm_delete(vm_name)
+    return redirect("/", code=302)
+
+
+@app.route('/clone/<vm_name>', methods=['GET'])
+def change_vm_clone(vm_name):
+    messages = []
+    # info = vm_info(vm_name)
+    # if info['state'] != 'powered off':
+    #     messages.append("Vm is running it will be turned off for change!")
+    return render_template('clone.html', vm_name=vm_name, messages=messages)
+
+
+@app.route('/clone/', methods=['POST'])
+def change_vm_clone_post():
+    vm_source = request.form['vm-source']
+    vm_new = request.form['vm-new']
+    # info = vm_info(vm_source)
+    # if info['state'] != 'powered off':
+    #     vm_stop(vm_source)
+    vm_clone(vm_source, vm_new)
+    return redirect("/", code=302)
+
 
 if __name__ == '__main__':
     app.config.from_object(__name__)
--- hw1/utils.py
+++ hw1/utils.py
@@ -14,9 +14,34 @@ def vm_info(vm_name):
     result = dict()
     result['name'] = vm_name
     result['cpu'] = next(re.finditer(r"Number of CPUs:\s+(\d+)", cmd_response, re.MULTILINE))[1]
-    result['state'] = next(re.finditer(r"State:\s+(.+)", cmd_response, re.MULTILINE))[1]
+    state = next(re.finditer(r"State:\s+(.+) \((.+)\)", cmd_response, re.MULTILINE))
+    result['state'], result['state_since'] = state[1], state[2]
     result['memory'] = next(re.finditer(r"Memory size\s+(.+)", cmd_response, re.MULTILINE))[1]
     return result
+
+
+def vm_start(vm_name):
+    return os.popen(f'vboxmanage startvm {vm_name} --type headless').read()
+
+
+def vm_stop(vm_name):
+    return os.popen(f'vboxmanage controlvm {vm_name} poweroff soft').read()
+
+
+def vm_change_cpu(vm_name, cores):
+    return os.popen(f'vboxmanage modifyvm {vm_name} --cpus {cores}').read()
+
+
+def vm_change_memory(vm_name, memory):
+    return os.popen(f'vboxmanage modifyvm {vm_name} --memory {memory}').read()
+
+
+def vm_delete(vm_name):
+    return os.popen(f'vboxmanage unregistervm {vm_name} --delete').read()
+
+
+def vm_clone(vm_source, vm_new):
+    return os.popen(f'vboxmanage clonevm {vm_source} --name={vm_new} --register').read()
 
 
 if __name__ == '__main__':
